a99f13aeeebf4d6eb218f2bbf338efe82c64ecf4
ELIPTUS <eliptus@gmail.com>
2010-05-21 02:13:49 -0600

    Use "/>" to close empty HTML element tags.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<??>?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
@@ -122,8 +123,7 @@ while ( 0 < count($acBgInfo) )
   <body>
     <div class="SplashContainer" >
       <img class="SplashContent" id="ELIPTUS" alt="ELIPTUS" src="" />
-      <span class="SplashVerticalSpacer" >
-      </span>
+      <span class="SplashVerticalSpacer" />
     </div>
   </body>
 </html>
